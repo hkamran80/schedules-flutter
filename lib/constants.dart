651fff0c397ac70a21fdf623681571051a699df1
H. Kamran <hkamran@hkamran.com>
2022-11-16 22:10:02 -0800

    Remove new issue link

--- lib/constants.dart
+++ lib/constants.dart
@@ -9,8 +9,6 @@ Uri feedbackLink = Uri.parse("https://go.unisontech.org/sch-mobile-feedback");
 
 Uri repositoryLink =
     Uri.parse("https://github.com/hkamran80/schedules-flutter");
-Uri newIssueLink =
-    Uri.parse("https://github.com/hkamran80/schedules-flutter/issues/new");
 
 Uri tosLink = Uri.parse("https://unisontech.org/legal/terms");
 Uri privacyPolicyLink = Uri.parse("https://unisontech.org/legal/privacy");
