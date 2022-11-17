d18af328e23319ff54d3f1f4244f6be56bb43dbd
H. Kamran <hkamran@hkamran.com>
2022-11-16 22:08:53 -0800

    Add feedback link

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,7 @@ Uri schedulesLink =
     Uri.parse("https://raw.unisontech.org/schedules/$schedulesVersion");
 
 Uri requestLink = Uri.parse("https://go.unisontech.org/schreq");
+Uri feedbackLink = Uri.parse("https://go.unisontech.org/sch-mobile-feedback");
 
 Uri repositoryLink =
     Uri.parse("https://github.com/hkamran80/schedules-flutter");
